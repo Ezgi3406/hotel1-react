603397f11f91dc54da71a41b5980ebbc387706d5
ezgi <ezgigunes3406@gmail.com>
2022-04-01 15:22:14 +0100

    lesson-2 completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-
-import Bookings from "./Bookings";
-import "./App.css";
+import Heading from "./Heading";
+import TouristInfoCard from "./TouristInfoCard";
+import Footer from "./Footer";
+import Restaurant from "./Restaurant";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">CYF Hotel</header>
+      <Heading />
+      <TouristInfoCard cards={cards} />
       <Bookings />
+      <Restaurant />
+      <Footer 
+        arrayOfProp={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
     </div>
   );
 };
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Order from "./Order";
 
 const Restaurant = () => {
   const pizzas = 0;
@@ -8,7 +9,10 @@ const Restaurant = () => {
       <ul>
         <li>
           Pizzas: {pizzas} <button className="btn btn-primary">Add</button>
-        </li>
+          </li>
+          <Order orderType="Pizzas" />
+          <Order orderType="Carrot-cake" />
+          <Order orderType="Margaritta" />
       </ul>
     </div>
   );
